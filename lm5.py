40e74067f77915e86f2e5a8c0945adac101e2519
swayam5342 <swayamsb50@gmail.com>
2024-11-10 02:32:42 +0530

    modified

--- lm5.py
+++ lm5.py
@@ -9,6 +9,8 @@ calculator:Callable[[float,float,str],float | Literal['Undefined']] = lambda x, 
     x / y if op == "/" and y != 0 else
     "Undefined"
 )
+
+
 print(calculator(10, 5, "+"))  
 print(calculator(10, 5, "*"))
 print(calculator(10, 0, "/"))
